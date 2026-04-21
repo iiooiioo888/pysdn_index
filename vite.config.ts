a8f02c06e0a5704d4574a1323bd296328968b610
iiooiioo888 <30275577+iiooiioo888@users.noreply.github.com>
2026-04-21 17:50:26 +0800

    fix: add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/pysdn_index/',
   plugins: [react()],
   server: {
     port: 5173,
